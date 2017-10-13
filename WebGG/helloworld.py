822349c6df1aef71e88b9a72cc19f2f320d193da
pochun-shih <awp5959@yahoo.com.tw>
2017-10-13 11:48:47 +0800

    upload web

---
+++ WebGG/helloworld.py
@@ -0,0 +1,62 @@
+"""from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+
+class MainPage(webapp.RequestHandler):
+    
+    
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp World!')
+
+
+app = webapp.WSGIApplication([('/', MainPage)], debug=True)
+
+
+def main():
+    run_wsgi_app(app)
+
+if __name__ == "__main__":
+    main()
+"""
+# [START app]
+import logging
+
+# [START imports]
+from flask import Flask, render_template, request
+# [END imports]
+
+# [START create_app]
+app = Flask(__name__)
+# [END create_app]
+
+
+@app.route('/')
+def form():
+    return render_template('index.html')
+"""
+# [START submitted]
+#@app.route('/submitted', methods=['POST'])
+def submitted_form():
+    name = request.form['name']
+    email = request.form['email']
+    site = request.form['site_url']
+    comments = request.form['comments']
+
+    # [END submitted]
+    # [START render_template]
+    return render_template(
+        'submitted_form.html',
+        name=name,
+        email=email,
+        site=site,
+        comments=comments)
+    # [END render_template]
+"""
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
+# [END app]
